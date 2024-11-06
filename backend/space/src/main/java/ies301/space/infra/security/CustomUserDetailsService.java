99b01eb71f7e555f27cae0873c4ea585b55ed311
edivaldolluisb <edivaldoluisb16@gmail.com>
2024-11-06 16:49:30 +0000

    FEAT: user resgiter and login

---
+++ backend/space/src/main/java/ies301/space/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package ies301.space.infra.security;
+
+import ies301.space.entities.user.User;
+import ies301.space.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
