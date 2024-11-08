07c34f37834a6d761581cd7eaf651f5813eafe68
Shelton Lázio Agostinho <sheltonagostinho@ua.pt>
2024-11-08 15:20:55 +0000

    endpoints começados em /api/v1

--- backend/space/src/main/java/ies301/space/controllers/HistoryController.java
+++ backend/space/src/main/java/ies301/space/controllers/HistoryController.java
@@ -1,6 +1,7 @@
 package ies301.space.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import ies301.space.entities.HistoryLaunch;
 import ies301.space.services.HistoryService;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("history")
+@RequestMapping("/api/v1")
 public class HistoryController {
 
     private final HistoryService historyService;
@@ -26,18 +26,18 @@ public class HistoryController {
         this.historyService = historyService;
     }
 
-    @GetMapping("/launches")
+    @GetMapping("/history/launches")
     public ResponseEntity<List<HistoryLaunch>> getAllHistoryLaunchs() {
         return new ResponseEntity<>(historyService.getAllHistory(), HttpStatus.OK);
     }
 
-    @GetMapping("/launches/{id}")
+    @GetMapping("/history/launches/{id}")
     public ResponseEntity<HistoryLaunch> getHistoryById(@PathVariable("id") Long historyId) {
         return new ResponseEntity<>(historyService.getHistoryById(historyId), HttpStatus.OK);
     }
 
     // só para testar
-    @PostMapping("/launches")
+    @PostMapping("/history/launches")
     public ResponseEntity<HistoryLaunch> createHistory(@RequestBody HistoryLaunch historyLaunch) {
         return new ResponseEntity<>(historyService.saveHistory(historyLaunch), HttpStatus.CREATED);
     }
