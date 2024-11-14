7d4c75526659898979ffaf4862c898b251b0ab8f
Edivaldo Bonfim <edivaldoluisb16@gmail.com>
2024-11-14 13:00:05 +0000

    FIX: update cors configurations

--- backend/space/src/main/java/ies301/space/controllers/AlertController.java
+++ backend/space/src/main/java/ies301/space/controllers/AlertController.java
@@ -5,22 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import ies301.space.entities.Alert;
 import ies301.space.entities.Launch;
 import ies301.space.services.AlertService;
 import jakarta.validation.Valid;
 
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1")
 public class AlertController {
     
