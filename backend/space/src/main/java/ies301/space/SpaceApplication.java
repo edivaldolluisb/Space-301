b9ad0485aac59e6c3d8e1d857f7c37a9f364bd80
Edivaldo Bonfim <edivaldoluisb16@gmail.com>
2024-11-27 23:22:46 +0000

    FEAT: add spring boot rabbitmq configs

--- backend/space/src/main/java/ies301/space/SpaceApplication.java
+++ backend/space/src/main/java/ies301/space/SpaceApplication.java
@@ -3,10 +3,11 @@ package ies301.space;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 
+@EnableRabbit
 @SpringBootApplication
 @EnableScheduling
 @OpenAPIDefinition(info = @Info(title = "Space API", version = "1.0", description = "Documentação da API"))
