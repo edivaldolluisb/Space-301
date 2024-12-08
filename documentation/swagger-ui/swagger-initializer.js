92f9bd718554cbfbed65cd8a389ba869b58c814d
Shelton Agostinho <sheltonagostinho@ua.pt>
2024-12-08 15:20:06 +0000

    documentacao offline

--- documentation/swagger-ui/swagger-initializer.js
+++ documentation/swagger-ui/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "./api-docs.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -15,7 +15,7 @@ window.onload = function() {
     ],
     layout: "StandaloneLayout" ,
 
-  "configUrl" : "/v3/api-docs/swagger-config",
+  // "configUrl" : "/v3/api-docs/swagger-config",
   "validatorUrl" : ""
 
   });
