9c97cdf297350f208542a5444672c7b3ed3b536c
Edivaldo Bonfim <edivaldoluisb16@gmail.com>
2024-12-09 20:16:39 +0000

    FEAT: reorganize DestinationAndDateHeader layout for improved readability and structure

--- frontend/space/src/components/destination-and-date-header.tsx
+++ frontend/space/src/components/destination-and-date-header.tsx
@@ -21,38 +21,38 @@ export function DestinationAndDateHeader() {
 
   return (
     <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+      <div className="flex items-center gap-2">
+        <MapPin className="size-5 text-zinc-400" />
+        <span className="text-zinc-100">{Address}</span>
+      </div>
+
+      <div className="flex items-center gap-5">
         <div className="flex items-center gap-2">
-          <MapPin className="size-5 text-zinc-400" />
-          <span className="text-zinc-100">{Address}</span>
+          <Calendar className="size-5 text-zinc-400" />
+          <span className="text-zinc-100">{displayedDate}</span>
         </div>
 
-        <div className="flex items-center gap-5">
-          <div className="flex items-center gap-2">
-            <Calendar className="size-5 text-zinc-400" />
-            <span className="text-zinc-100">{displayedDate}</span>
-          </div>
+        <div className="w-px h-6 bg-zinc-800" />
 
-          <div className="w-px h-6 bg-zinc-800" />
-
-          {/* <Button variant="secondary">
+        {/* <Button variant="secondary">
             Página inicial
             <Rocket className="size-5" />
           </Button> */}
-                
-      
+
+
         <Link to={"/dashboard"} className="">
           <Button variant="secondary">
             <Rocket className="size-5" />
             Página inicial
           </Button>
         </Link>
-      
 
-          <Button variant="secondary">
-            Definições
-            <Settings2 className="size-5" />
-          </Button>
-        </div>
+
+        <Button variant="secondary">
+          Definições
+          <Settings2 className="size-5" />
+        </Button>
       </div>
+    </div>
   )
 }
