b6e0e249defa30ed1cb97d5a74181451b6c493d7
Edivaldo Bonfim <edivaldoluisb16@gmail.com>
2024-12-12 05:26:07 +0000

    FEAT: add launch data fetching and integrate launch status display in RocketInfo component; enhance status mappings with dynamic color coding

--- frontend/space/src/pages/company/dashboard.tsx
+++ frontend/space/src/pages/company/dashboard.tsx
@@ -12,10 +12,7 @@ import {
   BatteryCharging,
   LucideIcon,
 } from 'lucide-react';
-import { LineChart, Line, XAxis, YAxis, ResponsiveContainer } from 'recharts';
-import { DestinationAndDateHeader } from './destination-and-date-header';
 import { Client } from '@stomp/stompjs';
-import { useNavigate } from 'react-router-dom';
 import { api } from "../../lib/axios";
 
 
@@ -91,9 +88,18 @@ interface Rocket {
 }
 interface RocketInfoProps {
   rocket: Rocket;
+  launch: Launch;
 }
 
-
+interface Launch {
+  id: number;
+  missionName: string;
+  launchDate: string;
+  rocketId: number;
+  address: string;
+  status: 'LAUNCHED' | 'SUCCESS' | 'FAILED' | 'PENDING';
+  astronauts: number[];
+};
 
 
 const unitMappings: { [key: string]: string } = {
@@ -112,20 +118,30 @@ const unitMappings: { [key: string]: string } = {
   energia_atual: 'J',
 };
 
-const statusMappings: { [key: string]: string } = {
-  altitude: 'Normal',
-  velocidade: 'Normal',
-  velocidade_x: 'Normal',
-  aceleracao: 'Normal',
-  forca_g: 'Normal',
-  pressao_atual: 'Normal',
-  temperatura_atual: 'Normal',
-  temperatura_motor_atual: 'Normal',
-  temperatura_externa_atual: 'Normal',
-  combustivel: 'Normal',
-  qualidade_atual: 'Normal',
-  oxigenio_atual: 'Normal',
-  energia_atual: 'Normal',
+const statusMappings = (parameter: string, value: number): string => {
+  const ranges: { [key: string]: { min: number; max: number } } = {
+    altitude: { min: 0, max: 100000 },
+    velocidade: { min: -666.67, max: 666.67 },
+    velocidade_x: { min: 0, max: 300 },
+    aceleracao: { min: -2.22, max: 2.22 },
+    forca_g: { min: -0.226, max: 0.226 },
+    pressao_atual: { min: 500, max: 101325 },
+    temperatura_atual: { min: 200, max: 288.15 },
+    temperatura_motor_atual: { min: 303.15, max: 600 },
+    temperatura_externa_atual: { min: 3, max: 288.15 },
+    combustivel: { min: 0, max: 2000000 },
+    qualidade_atual: { min: 80, max: 100 },
+    oxigenio_atual: { min: 18, max: 21 },
+    energia_atual: { min: 100, max: 50000 },
+  };
+
+  const range = ranges[parameter];
+
+  if (!range) {
+    return 'Irregular';
+  }
+
+  return value >= range.min && value <= range.max ? 'Normal' : 'Irregular';
 };
 
 const iconMappings: { [key: string]: LucideIcon } = {
@@ -144,6 +160,55 @@ const iconMappings: { [key: string]: LucideIcon } = {
   energia_atual: BatteryCharging,
 };
 
+
+const customIconBgColors: { [key: string]: string } = {
+  altitude: 'bg-blue-500/20',
+  velocidade: 'bg-blue-500/20',
+  velocidade_x: 'bg-blue-500/20',
+  aceleracao: 'bg-red-500/20',
+  forca_g: 'bg-green-500/20',
+  pressao_atual: 'bg-yellow-500/20',
+  temperatura_atual: 'bg-yellow-500/20',
+  temperatura_motor_atual: 'bg-yellow-500/20',
+  temperatura_externa_atual: 'bg-yellow-500/20',
+  combustivel: 'bg-red-500/20',
+  qualidade_atual: 'bg-green-500/20',
+  oxigenio_atual: 'bg-green-500/20',
+  energia_atual: 'bg-yellow-500/20',
+};
+
+const customIconTextColors: { [key: string]: string } = {
+  altitude: 'text-blue-500',
+  velocidade: 'text-blue-500',
+  velocidade_x: 'text-blue-500',
+  aceleracao: 'text-red-500',
+  forca_g: 'text-green-500',
+  pressao_atual: 'text-yellow-500',
+  temperatura_atual: 'text-yellow-500',
+  temperatura_motor_atual: 'text-yellow-500',
+  temperatura_externa_atual: 'text-yellow-500',
+  combustivel: 'text-red-500',
+  qualidade_atual: 'text-green-500',
+  oxigenio_atual: 'text-green-500',
+  energia_atual: 'text-yellow-500',
+};
+
+  // Define a cor do estado baseado no status
+  const getStatusColor = (status: string) => {
+    switch (status) {
+      case 'LAUNCHED':
+        return 'bg-green-500/20 text-green-500';
+      case 'SUCCESS':
+        return 'bg-green-500/20 text-green-500';
+      case 'FAILED':
+        return 'bg-red-500/20 text-red-500';
+      case 'PENDING':
+        return 'bg-yellow-500/20 text-yellow-500';
+      default:
+        return 'bg-gray-500/20 text-gray-500';
+    }
+  }
+
 // Componente Card base
 const Card: React.FC<CardProps> = ({ children, className = '' }) => (
   <div className={`bg-zinc-900 rounded-lg p-4 ${className}`}>
@@ -152,7 +217,7 @@ const Card: React.FC<CardProps> = ({ children, className = '' }) => (
 );
 
 // Componente de Informação do Foguete
-const RocketInfo: React.FC<RocketInfoProps> = ({ rocket }) => (
+const RocketInfo: React.FC<RocketInfoProps> = ({ rocket, launch }) => (
   <Card className="relative row-span-2">
     <div className="h-32 overflow-hidden rounded-lg mb-4">
       <img
@@ -173,8 +238,10 @@ const RocketInfo: React.FC<RocketInfoProps> = ({ rocket }) => (
         <MapPin className="w-5 h-5 text-gray-400" />
         <span>Aveiro, Portugal</span>
       </div>
-      <span className="px-2 py-1 bg-green-500/20 text-white-500 rounded text-sm">
-        In progress
+      <span className={`px-2 py-1 bg-green-500/20 text-white-500 rounded text-sm ${getStatusColor(
+            launch.status
+          )}`}>
+      {launch.status.toLowerCase()}
       </span>
     </div>
   </Card>
@@ -203,8 +270,13 @@ const StatusCard: React.FC<StatusCardProps> = ({
       <span className="text-4xl font-bold text-white">{Number(value).toFixed(2)}</span>
       <span className="text-gray-400">{unit}</span>
     </div>
-    {status && (
+
+    {status && status === 'Normal' ? (
       <span className="inline-block mt-2 px-2 py-1 bg-green-500/20 text-green-500 rounded text-sm">
+        {status}
+      </span>
+    ) : (
+      <span className="inline-block mt-2 px-2 py-1 bg-yellow-500/20 text-yellow-500 rounded text-sm">
         {status}
       </span>
     )}
@@ -217,6 +289,25 @@ const Dashboard: React.FC<DashboardProps> = ({ launchId }) => {
   const [rocketData, setRocketData] = useState(null as WebSocketData | null);
   const [rocket, setRocket] = useState(null as Rocket | null);
   const [error, setError] = useState<string | null>(null);
+  const [launch, setLaunch] = useState<Launch | null>(null);
+
+  useEffect(() => {
+    // Função de fetch dos dados do lançamento
+    const fetchLaunch = async () => {
+      try {
+        const response = await api.get(`/launches/${launchId}`);
+        const data: Launch = await response.data;
+        console.log('Launch:', data)
+        setLaunch(data);
+      } catch (error) {
+        console.error('Erro ao buscar dados do lançamento:', error);
+      } finally {
+        // setLoading(false);
+      }
+    };
+
+    fetchLaunch();
+  }, [launchId]);
 
   const fetchRocket = async () => {
     try {
@@ -271,23 +362,23 @@ const Dashboard: React.FC<DashboardProps> = ({ launchId }) => {
 
   if (!rocketData) return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
-      {!rocket ? <h1 className="text-zinc-500 text-sm">Nenhum lançamento cadastrado para essa data.</h1> : <RocketInfo rocket={rocket} />}
+      {!rocket ? <h1 className="text-zinc-500 text-sm">Nenhum lançamento cadastrado para essa data.</h1> : <RocketInfo rocket={rocket} launch={launch} />}
     </div>);
 
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {!rocket ? <h1 className="text-zinc-500 text-sm">Nenhum lançamento cadastrado para essa data.</h1> : <RocketInfo rocket={rocket} />}
+        {!rocket ? <h1 className="text-zinc-500 text-sm">Nenhum lançamento cadastrado para essa data.</h1> : <RocketInfo rocket={rocket} launch={launch} />}
         {Object.entries(rocketData).map(([key, value]) => {
           if (key === 'alertas' || key === 'alerta') return null;
           const title = titleMappings[key] || camelCaseToTitleCase(key);
 
 
           const unit = unitMappings[key] || '';
-          const status = statusMappings[key] || 'Normal';
+          const status = statusMappings(key, value as number);
           const Icon = iconMappings[key] || Flame;
-          const iconTextColor = 'text-blue-500';
-          const iconBgColor = 'bg-blue-500/20';
+          const iconTextColor = customIconTextColors[key] || 'text-red-500';
+          const iconBgColor = customIconBgColors[key] || 'bg-red-500/20';
 
           return (
             <StatusCard
