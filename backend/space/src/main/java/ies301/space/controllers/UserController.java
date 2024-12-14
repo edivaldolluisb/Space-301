a7df1baddcb15fbba8cc83e1233e19f232de2c7b
Edivaldo Bonfim <edivaldoluisb16@gmail.com>
2024-12-14 14:45:30 +0000

    FEAT: set default user role to USER when not provided in create and update user requests

--- backend/space/src/main/java/ies301/space/controllers/UserController.java
+++ backend/space/src/main/java/ies301/space/controllers/UserController.java
@@ -17,6 +17,8 @@ import ies301.space.services.UserService;
 import java.util.List;
 import ies301.space.dto.UserResponseDTO;
 
+import ies301.space.entities.user.UserRole;
+
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -50,6 +52,9 @@ public class UserController {
     
     @PostMapping
     public ResponseEntity<UserResponseDTO> createUser(@RequestBody User user) {
+        if (user.getRole() == null) {
+            user.setRole(UserRole.USER);
+        }
         UserResponseDTO createdUser = userService.saveUser(user);
         return ResponseEntity.ok(createdUser);
     }
@@ -63,6 +68,10 @@ public class UserController {
         if (updatedUser.getPassword() != null && !updatedUser.getPassword().isEmpty()) {
             
             updatedUser.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+        }
+        
+        if (updatedUser.getRole() == null) {
+            updatedUser.setRole(UserRole.USER);
         }
         
         UserResponseDTO user = userService.updateUser(id, updatedUser);
