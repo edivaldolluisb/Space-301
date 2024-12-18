50d1a599ee974aca0cd566944ec8cbf6cc6b8a6e
Shelton Lázio Agostinho <sheltonagostinho@ua.pt>
2024-12-18 22:13:28 +0000

    remove unnecessary endpoints

--- backend/space/src/main/java/ies301/space/repositories/HistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package ies301.space.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ies301.space.entities.HistoryLaunch;
-
-public interface HistoryRepository extends JpaRepository<HistoryLaunch,Long> {}  
